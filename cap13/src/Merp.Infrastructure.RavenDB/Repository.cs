da3e99de8a039b1906385706692a8f6d568f8d25
Andrea Saltarello <andysal@outlook.com>
2014-07-25 10:45:31 +0200

    started working on implementing a RavenDB powered Repository

---
+++ cap13/src/Merp.Infrastructure.RavenDB/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Merp.Infrastructure;
+using Raven.Client.Embedded;
+
+namespace Merp.Infrastructure.RavenDB
+{
+    public class Repository : IRepository
+    {
+        private static EmbeddableDocumentStore DocumentStore { get; set; }
+        static Repository()
+        {
+            DocumentStore = new EmbeddableDocumentStore
+            {
+                DataDirectory = "App_Data",
+                UseEmbeddedHttpServer = true
+            };
+        }
+
+        public T GetById<T>(Guid id) where T : IAggregate
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save<T>(T item) where T : IAggregate
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
